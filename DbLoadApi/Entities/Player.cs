6910f28413bef9082e80c97f03b59cb4f49055c7
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-01-05 10:40:48 -0300

    adicionando inventário

--- DbLoadApi/Entities/Player.cs
+++ DbLoadApi/Entities/Player.cs
@@ -6,6 +6,9 @@ namespace DbLoadApi.Entities
     public class Player : IEntity<int>
     {
         public int Id { get; set; }
+        public int Health { get; set; }
         public PlayerJob Job { get; set; }
+        public IEnumerable<PlayerInventory> Inventory { get; set; }
+        public IEnumerable<IEntity> ActiveItems => Inventory.Where(i => i.IsActive);
     }
 }
---
+++ DbLoadApi/Entities/PlayerInventory.cs
@@ -0,0 +1,10 @@
+﻿using DbLoadApi.Entities.Base;
+
+namespace DbLoadApi.Entities
+{
+    public class PlayerInventory : IEntity
+    {
+        public IEntity Item { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
