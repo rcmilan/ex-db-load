444018e2129b3a5f7f7cd6da6fea57e9ff3b4c13
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-01-05 13:18:02 -0300

    Mysql

--- DbLoadApi/Configurations/ModuleDependency.cs
+++ DbLoadApi/Configurations/ModuleDependency.cs
@@ -1,10 +1,21 @@
-﻿namespace DbLoadApi.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+
+namespace DbLoadApi.Configurations
 {
     public static class ModuleDependency
     {
 
         public static IServiceCollection AddServices(this IServiceCollection services, string connectionString)
         {
+
+            var mySqlConnection = new MySqlConnection(connectionString);
+
+            services
+                .AddDbContext<MyDbContext>(
+                    options => options.UseMySql(connectionString, ServerVersion.AutoDetect(mySqlConnection))
+                );
+
             return services;
         }
     }
--- DbLoadApi/Configurations/MyDbContext.cs
+++ DbLoadApi/Configurations/MyDbContext.cs
@@ -1,6 +1,29 @@
-﻿namespace DbLoadApi.Configurations
+﻿using DbLoadApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbLoadApi.Configurations
 {
-    public class MyDbContext
+    public class MyDbContext : DbContext
     {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Enchantment>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Equipment>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Player>()
+                .HasKey(p => p.Id);
+            
+            modelBuilder.Entity<Weapon>()
+                .HasKey(w => w.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
